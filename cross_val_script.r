5456238f6f85d5172301ac6ff599c92c67d84732
asafgibor <asafdrizlikh@gmail.com>
2017-02-07 13:34:57 -0600

    Add files via upload

--- cross_val_script.r
+++ cross_val_script.r
@@ -6,6 +6,7 @@ DTA <- read.csv("hof_data.csv")
 ## Extract a few offensive statistics (numerical variables).
 num_vars <- c("SP", "G", "H", "AB", "ASG", "R")
 X <- as.matrix(DTA[, num_vars])
+DTA_num <- DTA[, c("HOF", num_vars)]
 
 ## Variable declarations
 sens = NULL
@@ -16,26 +17,64 @@ train = NULL
 thresh_seq = seq(from = .05, to = .95, by = .05)
 n <- nrow(DTA)
 
+pred = matrix(0, nrow = n, ncol = 19)
+check = matrix(0, nrow = n, ncol = 19)
+
 ## Performs LOOCV 
-for (i in 1:n) {
-	test <- as.matrix(DTA[i,])
-	tr1 <- as.matrix(DTA[(0:(i-1)),])
-	tr2 <- as.matrix(DTA[((i+1):n),])
-    train <- as.data.frame(merge(tr1, tr2, by = "row.names", all = TRUE))
+for (j in 1:19) {
+	for (i in 1:n) {
+
+		test <- DTA_num[i,] # uses one element as a test
+		train <- DTA_num[-i,] # uses remainder of data elements as training
 	
-    lda_out <- lda(HOF ~ X, data = train)
-    lda_pred <- predict(lda_out, newdata = test)
+		lda_out <- lda(HOF ~., data = train)
+		lda_pred <- predict(lda_out, newdata = test)	
+		
+		# makes prediction based on current threshold value	
+		if (lda_pred$posterior[1] > thresh_seq[j]) {
+			pred[i,j] = 1
+		}
+		else {
+			pred[i,j] = 0
+		}
+		
+	}
+}
 	
-	for (j in 1:thresh_seq) {
-		lda_pred$posterior = thresh_seq[j]
-		
-		results <- as.matrix(table(lda_pred, DTA$HOF))
-		
-		sens[i,j] = results[1,1] / (results[1,1] + results[2,1])
-		spec[i,j] = results[1,2] / (results[1,2] + results[2,2])
-	    acc[i,j] = (sens[i,j] + spec[i,j]) / 2
+## Checks the pred matrix to see which values
+## were correctly predicted	
+for (j in 1:19) {
+predYesRight = 0 # reset number of HOF yes predicted correctly
+predYesWrong = 0
+predNoRight = 0
+predNoWrong = 0
+
+	for (i in 1:n) {
+		if (pred[i,j] == 1) { # if yes was predicted
+			if (DTA$HOF[i] == '1') { # compare to test case
+				check[i,j] = 0 # marks prediction as correct
+				predYesRight <- predYesRight + 1  # increase count
+			}
+			else {
+				check[i,j] = 1 # marks prediction as incorrect
+				predYesWrong <- predYesWrong + 1  
+			}
+		}
+		if (pred[i,j] == 0) { # if no was predicted
+			if (DTA$HOF[i] == '0') {
+				check[i,j] = 0
+				predNoRight <- predNoRight + 1  
+			}
+			else {
+				check[i,j] = 1		
+				predNoWrong <- predNoWrong + 1  				
+			}
+		}
 	}
 	
+	sens[j] = predYesRight / (predYesRight + predYesWrong) # calculation for sensitivity 
+	spec[j] = predNoWrong / (predNoWrong + predNoRight) # calculation for specificity 
+	acc[j] = (sens[j] + spec[j]) / 2 # calculation for balanced accuracy
 }
 
 ## can use sens, spec, and acc to display info
