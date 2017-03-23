58f0e0be0ad3a33e241c93d59f9b51e1ae5756cf
asafgibor <asafdrizlikh@gmail.com>
2017-03-23 14:53:50 -0500

    Add files via upload

---
+++ leaps.r
@@ -0,0 +1,9 @@
+library(leaps)
+
+## Load data
+DTA <- read.csv("norm_hof.csv")
+
+y <- as.matrix(DTA[, c("HOF")])
+x <- as.matrix(DTA[, c(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25)])
+
+analysis <- leaps(x, y)
