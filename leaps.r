bc9634db904320099dfc130c45b9111b8dda7a80
stevebroll <stevebroll@tamu.edu>
2017-03-23 15:34:54 -0500

    Update leaps.r

--- leaps.r
+++ leaps.r
@@ -3,7 +3,10 @@ library(leaps)
 ## Load data
 DTA <- read.csv("norm_hof.csv")
 
-y <- as.matrix(DTA[, c("HOF")])
-x <- as.matrix(DTA[, c(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25)])
 
-analysis <- leaps(x, y)
+##y <- DTA[, c("HOF")]
+###x <- DTA[, c(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25)]
+X = data.matrix(DTA[3:25))
+y = data.matrix(DTA['HOF'])
+
+analysis <- leaps(x, y, nbest = 1, method = "r2")
