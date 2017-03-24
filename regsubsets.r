ed14f45d1863fdcaabbc9b1f8c695528ebcfca7d
asafgibor <asafdrizlikh@gmail.com>
2017-03-24 00:59:15 -0500

    Add files via upload

--- regsubsets.r
+++ regsubsets.r
@@ -3,7 +3,7 @@ library(leaps)
 ## Load data
 DTA <- read.csv("norm_hof.csv")
 
-X = data.matrix(DTA[3:25))
+X = data.matrix(DTA[3:25])
 y = data.matrix(DTA['HOF'])
-analysis_rs <- regsubsets(x=x,y=y,data=DTA,nbest=1,method=c("exhaustive", "backward", "forward", "seqrep",matrix=TRUE, matrix.logical = FALSE,)
-summary(analysis_rs,all.best=TRUE,matrix=TRUE,matrix.logical=FALSE)
+analysis_rs <- regsubsets(x=x,y=y,data=DTA,nbest=1,method=c("exhaustive", "backward", "forward", "seqrep",matrix=TRUE, matrix.logical = FALSE))
+a_rs <- summary(analysis_rs,all.best=TRUE,matrix=TRUE,matrix.logical=FALSE)
